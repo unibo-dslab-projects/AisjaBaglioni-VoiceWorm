74ca8bdfba2eef4df87ca21086be376bca941e54
Aisja Baglioni <aisjabaglioni@gmail.com>
2026-01-22 01:00:31 +0100

    link fix

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 <template>
     <footer>
-      <p>♡ Coded with love by <a class="aisja-link" href="https://aisja.it">Aisja</a>, 2026 ♡</p>
+      <p>♡ Coded with love by <a class="aisja-link" target="_blank" href="https://aisja.it">Aisja</a>, 2026 ♡</p>
     </footer>
 </template>
 
